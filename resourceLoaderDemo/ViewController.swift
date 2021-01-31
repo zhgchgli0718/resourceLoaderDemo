b0571d8af3fb94c10c24c0ea84fa10f027a5525b
zhgchgli <zhgchgli@gmail.com>
2021-01-31 15:30:10 +0800

    Initial Commit

---
+++ resourceLoaderDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  resourceLoaderDemo
+//
+//  Created by Zhong Cheng Li on 2021/1/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
